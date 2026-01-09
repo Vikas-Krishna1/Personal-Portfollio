b000ba70902de1b7b82783359d457c266b5e9cba
Vikas Krishna <vikas.krishna200007@gmail.com>
2026-01-09 17:54:49 -0600

    Finished App

--- personal_portfollio/urls.py
+++ personal_portfollio/urls.py
@@ -23,6 +23,7 @@ from portfollio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
+    path('project/<int:pk>',views.project_detail,name="project_detail"),
     
 ]
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfollio/views.py
+++ portfollio/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 # Create your views here.
 def home(request):
 	projects = Project.objects.all()
 	return render(request, "portfollios/home.html", {'projects':projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, "portfollios/detail.html", {'project': project})
