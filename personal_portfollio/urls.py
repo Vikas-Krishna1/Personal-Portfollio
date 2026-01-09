e316c76a6c4835b8e83724de3827a0e4fdada756
Vikas Krishna <vikas.krishna200007@gmail.com>
2026-01-08 18:14:25 -0600

    No Blogs

--- personal_portfollio/urls.py
+++ personal_portfollio/urls.py
@@ -23,7 +23,7 @@ from portfollio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
-    path('blog/',include('blog.urls'))
+    
 ]
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
