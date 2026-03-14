fd6bf34c928986e6cce14ba40b80a1728c6ddb68
Vikas-Krishna1 <150981669+Vikas-Krishna1@users.noreply.github.com>
2026-03-14 02:50:34 -0500

    Fixedd

--- portfollio/views.py
+++ portfollio/views.py
@@ -4,9 +4,9 @@ from .models import Project
 # Create your views here.
 def home(request):
 	projects = Project.objects.all()
-	return render(request, "portfollios/home.html", {'projects':projects})
+	return render(request, "portfollio/home.html", {'projects':projects})
 
 
 def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
-    return render(request, "portfollios/detail.html", {'project': project})
+    return render(request, "portfollio/detail.html", {'project': project})
