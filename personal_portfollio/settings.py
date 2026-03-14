c2b013467f08106294311721646d2448b0995534
Vikas-Krishna1 <150981669+Vikas-Krishna1@users.noreply.github.com>
2026-03-14 03:43:09 -0500

    Whitenoise

--- personal_portfollio/settings.py
+++ personal_portfollio/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = (
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
-)
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
