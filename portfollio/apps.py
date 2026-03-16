c93d650c41b91185e06c664002fd3decb4b1d244
Vikas-Krishna1 <150981669+Vikas-Krishna1@users.noreply.github.com>
2026-03-16 13:30:38 -0500

    Fxed up

--- portfollio/apps.py
+++ portfollio/apps.py
@@ -4,18 +4,3 @@ from django.apps import AppConfig
 class PortfollioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfollio'
-from django.apps import AppConfig
-
-class PortfollioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portfollio'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        
-        if not User.objects.filter(username="Vikas-Dev").exists():
-            User.objects.create_superuser(
-                username="Vikas-Dev",
-                email="your@email.com",
-                password="08282007Vk"
-            )
